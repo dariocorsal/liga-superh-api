2f076e2b07cd714fcfdb3ed4b1b2ae1acd00e5d7
dariocorsal <dariocscs@gmail.com>
2025-02-24 22:07:22 -0800

    Conexion a Mongo Atlas

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-
-require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Conectado a MongoDB Atlas"))
+  .catch((err) => console.error("Error al conectar a MongoDB:", err));
 
 app.get("/", (req, res) => {
   res.send("API corriendo correctamente");
