a2caf9f8acfde9b61fe9291b264d4d2860bb7775
dariocorsal <dariocscs@gmail.com>
2025-02-25 15:57:07 -0800

    Quitar debugging

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ app.use("/liga", ligaRoutes);
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.MONGO_URI);
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Conectado a MongoDB Atlas"))
